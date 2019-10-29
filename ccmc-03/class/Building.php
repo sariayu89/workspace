cf16bee8218ca20f532a4b13172f37b79042ec03
sariayu89 <sariayu.usman93@gmail.com>
2019-10-29 05:46:43 +0000

    20191029

---
+++ ccmc-03/class/Building.php
@@ -0,0 +1,33 @@
+<?php
+class Building {
+    // properties
+    private $name;
+    private $height;
+    
+    // cosntructor
+    function  __construct($name, $height) {
+        $this->name = $name;
+        $this->height = $height;
+    }
+    
+    // accessor method
+    function setName($name) {
+        $this->name = $name;
+    }
+    function getName() {
+        return $this->name;
+    }
+    function setHeight($height) {
+        $this->height = $height;
+    }
+    function setHeight($height) {
+        return $this->height;
+    }
+    
+    function getProfile () {
+        $profile ="{$this->name} の高さは {$this->heigtht} ｍです。";
+        return $profile;
+    }
+    }
+}
+?>
